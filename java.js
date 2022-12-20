8f4ad9fd08a9a85e623b2db287308ed49613e204
Kertész Ruben <70895461+KecsaposKrumpli2@users.noreply.github.com>
2022-12-20 11:08:50 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function cserekek() {
+    document.getElementById("kep").src="img/m3blue.jpg";
+}
+
+function cserepiros() {
+    document.getElementById("kep").src="img/m3red.jpg";
+}
+
+function cserefeher() {
+    document.getElementById("kep").src="img/m3white.jpg";
+}
